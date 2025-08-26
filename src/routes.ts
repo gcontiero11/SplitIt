8e81f9655b2aa5b8e423324942f62c56ee1fff50
gcontiero11 <gustcontiero@gmail.com>
2025-08-25 21:58:08 -0300

    feat: adding swagger for a better documentation and adding user routes

---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+import { FastifyInstance } from "fastify";
+import { userRoutes } from "./user/user.route";
+import { FastifyTypedInstance } from "./types";
+
+async function routes(app: FastifyTypedInstance) {
+  app.register(userRoutes);
+}
+
+export { routes };
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,44 @@
 import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
+import {
+  validatorCompiler,
+  serializerCompiler,
+  jsonSchemaTransform,
+} from "fastify-type-provider-zod";
+import { fastifySwagger } from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+import { routes } from "./routes";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
-app.get("/", async () => {
-  return { message: "Hello, world!" };
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifyCors, {
+  origin: "*",
+});
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "SplitIt API",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
+
+app.register(routes);
+
+app.listen({ port: 3000, host: "0.0.0.0" }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server is running at ${address}`);
 });
